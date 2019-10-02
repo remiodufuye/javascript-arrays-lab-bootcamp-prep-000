49c1c41be3b61d4801eaad97f89994a5095616e5
remi.odufuye <remi.odufuye@gmail.com>
2019-10-02 14:31:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] 
+
+//define your array here
 
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
+  
+  
   
 }
