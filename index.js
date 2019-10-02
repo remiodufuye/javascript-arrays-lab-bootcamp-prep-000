3039f40d6d39294bd32bc832ece75e925fb7b794
remi.odufuye <remi.odufuye@gmail.com>
2019-10-02 15:03:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,8 +48,12 @@ function prependKitten(name) {
 
 
 function removeLastKitten() {
-  
+  kittens.slice(0, kittens.length - 1) ;
+  return kittens ; 
 }
+
+
+ 
 
 function removeFirstKitten() {
   
